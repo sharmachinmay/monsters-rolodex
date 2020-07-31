d5c2c1fb1edac1bd087657786535b1f5e387cc4f
unknown <sharmaceaser5678@gmail.com>
2020-08-01 01:54:57 +0530

    Updates

---
+++ precache-manifest.b547ffeffbc33101cf9076841c511149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e6165c895916670e8836d6f5524b36",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0fd1c0c0682bc9048dfd",
+    "url": "/monsters-rolodex/static/css/main.4460311d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd1c0c0682bc9048dfd",
+    "url": "/monsters-rolodex/static/js/main.1fe9dd62.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
